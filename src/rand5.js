ce23a9e9a0a5d705505ead0aafc53d3705f3f0bc
koraytugay <koray@tugay.biz>
2019-07-28 20:28:16 -0400

    Refactoring

--- src/rand5.js
+++ src/rand5.js
@@ -1,9 +1,13 @@
 // Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, 
 // implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+function rand7() {
+    return Math.floor(Math.random() * Math.floor(6)) + 1;
+}
+
 const Rand5 = {
-    random : () => {
-        const random = Math.floor(Math.random() * Math.floor(6)) + 1;
-        return random > 5 ? Rand5.random() : random;
+    random5 : () => {
+        const random = rand7();
+        return random > 5 ? Rand5.random5() : random;
     }
 };
 
--- tests/rand5test.js
+++ tests/rand5test.js
@@ -5,12 +5,9 @@ let freq = [];
 freq.length = 6;
 freq.fill(0);
 
-for (let i = 0; i < 100000; i++) {
-    const number = Rand5.random();
-    freq[number]++;
-}
+for (let i = 0; i < 100000; i++)
+    freq[Rand5.random5()]++;
 
 expect(freq[0]).to.equal(0);
-for (let i = 1; i < 6; i++) {
+for (let i = 1; i < 6; i++)
     expect(freq[i]).to.greaterThan(19000);
-}
