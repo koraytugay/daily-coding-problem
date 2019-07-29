ac55cf10ba455a09cb3dc5e7478ab569ddb56c10
koraytugay <koray@tugay.biz>
2019-07-28 20:33:28 -0400

    Whitespace

--- src/two_sigma/rand5.js
+++ src/two_sigma/rand5.js
@@ -1,5 +1,6 @@
-// Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability, 
+// Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform probability,
 // implement a function rand5() that returns an integer from 1 to 5 (inclusive).
+
 function rand7() {
     return Math.floor(Math.random() * Math.floor(6)) + 1;
 }
