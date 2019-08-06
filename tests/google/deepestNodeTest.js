9c17fef07db345f36b6e3b15f37dcee96da49fa1
koraytugay <koray@tugay.biz>
2019-08-06 06:57:32 -0400

    DeepestNode -> shorter

--- tests/google/deepestNodeTest.js
+++ tests/google/deepestNodeTest.js
@@ -1,17 +1,7 @@
 const expect = require('chai').expect;
 const deepestNode = require('../../src/google/deepestNode');
 
-let a = deepestNode.TreeNode('a');
-let b = deepestNode.TreeNode('b');
-let c = deepestNode.TreeNode('c');
-let d = deepestNode.TreeNode('d');
-
-a.left = b;
-b.left = d;
-
-a.right = c;
-
+let a = deepestNode.TreeNode('a', deepestNode.TreeNode('b', deepestNode.TreeNode('d')), deepestNode.TreeNode('c'));
 expect(deepestNode.deepestNode(a).node.val).eq('d');
 
-a = deepestNode.TreeNode('a');
-expect(deepestNode.deepestNode(a).node.val).eq('a');
+expect(deepestNode.deepestNode(deepestNode.TreeNode('one')).node.val).eq('one');
