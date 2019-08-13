7b1c17c40d6b95a1197558cf9c97d8a696230db2
koraytugay <koray@tugay.biz>
2019-08-13 19:00:09 -0400

    Snapchat - Overlapping Intervals

--- src/snapchat/overlappingIntervals.js
+++ src/snapchat/overlappingIntervals.js
@@ -5,16 +5,12 @@
 // Given [(1, 3), (4, 10), (5, 8), (20, 25)], you should return
 //       [(1, 3), (4, 10), (20, 25)].
 exports.overlapping = function (intervals) {
-    function containedBy(interval, other) {
-        return interval[0] <= other[0] && interval[1] >= other[1];
-    }
-
+    const containedBy = (interval, other) => interval[0] <= other[0] && interval[1] >= other[1];
     const indicesToRemove = new Set();
     for (let i = 0; i < intervals.length - 1; i++)
         for (let j = i + 1; j < intervals.length; j++) {
             if (containedBy(intervals[i], intervals[j])) indicesToRemove.add(j);
             if (containedBy(intervals[j], intervals[i])) indicesToRemove.add(i);
         }
-
     return intervals.filter((value, index) => !indicesToRemove.has(index));
 };
