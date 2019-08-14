f36fb8d282703a3cf647a82e9ec975daa63ab942
koraytugay <koray@tugay.biz>
2019-08-14 14:36:18 -0400

    Perfect Number tests

--- tests/microsoft/perfectTest.js
+++ tests/microsoft/perfectTest.js
@@ -4,3 +4,4 @@ const perfect = require('../../src/microsoft/perfect');
 expect(perfect.findPerfectNumber(1)).eq(19);
 expect(perfect.findPerfectNumber(2)).eq(28);
 expect(perfect.findPerfectNumber(3)).eq(37);
+expect(perfect.findPerfectNumber(15)).eq(154);
