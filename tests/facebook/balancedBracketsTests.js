621a0470123d397627f30c6cafe764f8b8126147
koraytugay <koray@tugay.biz>
2019-08-15 21:10:42 -0400

    Facebook - Balanced Brackets

--- tests/facebook/balancedBracketsTests.js
+++ tests/facebook/balancedBracketsTests.js
@@ -3,9 +3,12 @@ const balancedBrackets = require('../../src/facebook/balancedBrackets');
 
 expect(balancedBrackets.isBracketsBalanced("[]")).to.be.true;
 expect(balancedBrackets.isBracketsBalanced("[")).to.be.false;
+expect(balancedBrackets.isBracketsBalanced("]")).to.be.false;
 expect(balancedBrackets.isBracketsBalanced("[[")).to.be.false;
 expect(balancedBrackets.isBracketsBalanced("([])[]({})")).to.be.true;
 expect(balancedBrackets.isBracketsBalanced("((()")).to.be.false;
+expect(balancedBrackets.isBracketsBalanced("()))")).to.be.false;
 expect(balancedBrackets.isBracketsBalanced("[{([])}]")).to.be.true;
 expect(balancedBrackets.isBracketsBalanced("[{(")).to.be.false;
 expect(balancedBrackets.isBracketsBalanced("([])))")).to.be.false;
+expect(balancedBrackets.isBracketsBalanced("]]]]")).to.be.false;
