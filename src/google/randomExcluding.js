998553d19fba6a90548c2e4d3d0700425fa2a12a
koraytugay <koray@tugay.biz>
2019-08-16 15:29:08 -0400

    Google - Random excluding list

--- src/google/randomExcluding.js
+++ src/google/randomExcluding.js
@@ -1,4 +1,6 @@
-exports.random = function randomExcluding(n, /*array*/ excluding) {
+// Given an integer n and a list of integers l,
+// write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
+exports.random = function randomExcluding(n, /*array*/ l) {
     let number = Math.floor(Math.random() * Math.floor(n));
-    return new Set(excluding).has(number) ? randomExcluding(n, excluding) : number;
+    return new Set(l).has(number) ? randomExcluding(n, l) : number;
 };
