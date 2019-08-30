c04b4e2a295e062f2384fd451dc65ecd4e3587bf
koraytugay <koray@tugay.biz>
2019-08-29 21:19:29 -0400

    Intersecting Notes - More tests.

--- tests/google/intersectingNodesTest.js
+++ tests/google/intersectingNodesTest.js
@@ -6,8 +6,10 @@ let listOne = reverseLinkedList.ListNode(0, reverseLinkedList.ListNode(1, revers
 let listTwo = reverseLinkedList.ListNode(4, reverseLinkedList.ListNode(3, reverseLinkedList.ListNode(2, undefined)));
 
 expect(intersectingNodes.intersectingNode(listOne, listTwo)).to.equal(2);
+expect(intersectingNodes.intersectingNode(listTwo, listOne)).to.equal(2);
 
 listOne = reverseLinkedList.ListNode(0, reverseLinkedList.ListNode(1, reverseLinkedList.ListNode(2, undefined)));
 listTwo = reverseLinkedList.ListNode(3, reverseLinkedList.ListNode(4, reverseLinkedList.ListNode(5, undefined)));
 
 expect(intersectingNodes.intersectingNode(listOne, listTwo)).to.equal(-1);
+expect(intersectingNodes.intersectingNode(listTwo, listOne)).to.equal(-1);
