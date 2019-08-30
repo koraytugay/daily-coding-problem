9c4c43a8f0da1c8acbecc46ded7ba3d6cd242988
koraytugay <koray@tugay.biz>
2019-08-29 21:22:27 -0400

    Numbers Add Up - Shorter.

--- tests/google/numbersAddUpTest.js
+++ tests/google/numbersAddUpTest.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
-const numbersAddUpOrNot = require('../../src/google/numbersAddUp.js');
+const addsUp = require('../../src/google/numbersAddUp.js');
 
 let numbers = [10, 15, 3, 7];
 
-assert.isTrue(numbersAddUpOrNot.addsUp(numbers, 17));
-assert.isTrue(numbersAddUpOrNot.addsUp(numbers, 25));
-assert.isFalse(numbersAddUpOrNot.addsUp(numbers, 90));
+assert.isTrue(addsUp(numbers, 17));
+assert.isTrue(addsUp(numbers, 25));
+assert.isFalse(addsUp(numbers, 90));
